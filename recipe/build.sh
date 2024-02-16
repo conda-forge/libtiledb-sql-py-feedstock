284c470112b6e87ad2fb84b35b94a6e1a5fede16
dimitrisstaratzis <jstaratzi@icloud.com>
2024-02-16 16:52:22 +0200

    add include path for ```mysql.h``` manually

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@
 set -e
 set -x
 
+C_INCLUDE_PATH="/usr/include/mysql:$C_INCLUDE_PATH" ## for C compiler
+export C_INCLUDE_PATH
+
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
