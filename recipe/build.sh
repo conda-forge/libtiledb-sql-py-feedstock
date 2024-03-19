f1555ce6da128e629b294fbdceb862d2f081bd7b
Dimitris Staratzis <33267511+DimitrisStaratzis@users.noreply.github.com>
2024-03-19 21:06:21 +0200

    add ```-Wno-error=implicit-function-declaration``` in Apple Silicon (#27)
    * add ```-Wno-error=implicit-function-declaration``` in Apple Silicon
    
    * MNT: Re-rendered with conda-build 24.1.2, conda-smithy 3.32.0, and conda-forge-pinning 2024.03.19.15.37.47
    
    * revert build number bump
    
    * bump build number
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,10 @@
 set -e
 set -x
 
+FLAGS_NEEDED="-Wno-error=implicit-function-declaration"
+if [[ $target_platform == osx-arm64  ]]; then
+  export CFLAGS="${CFLAGS-} ${FLAGS_NEEDED}"
+  export CXXFLAGS="${CXXFLAGS-} ${FLAGS_NEEDED}"
+fi
+
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
