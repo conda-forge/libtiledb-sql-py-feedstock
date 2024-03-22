c3622eeee2d7c63b33e0cf1dee328ee86c372d29
Dimitris Staratzis <33267511+DimitrisStaratzis@users.noreply.github.com>
2024-03-22 14:38:16 +0200

    try including mysql.h with <>

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,4 +14,12 @@ pwd;
 echo '=================================================================================';
 ls -l;
 
+# debug
+sed -i -e 's/#include "mysql.h"/#include <mysql.h>/g' tiledb/sql/_mysql.c
+sed -i -e 's/#include "mysqld_error.h"/#include <mysqld_error.h>/g' tiledb/sql/_mysql.c
+
+echo "Replacement complete."
+
+cat tiledb/sql/_mysql.c;
+
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
